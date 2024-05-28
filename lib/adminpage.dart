fac9971c3f1ef30bda267bcf27e9680ecfee3ac3
AlbertoG <luis.betoxki@gmail.com>
2024-05-28 09:44:19 -0600

    implementaciones 28/05

--- lib/adminpage.dart
+++ lib/adminpage.dart
@@ -1,16 +1,274 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class AdminPage extends StatelessWidget {
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
+import 'package:printing/printing.dart';
+
+import 'estadistica.dart';
+
+class AdminPage extends StatefulWidget {
+  @override
+  _AdminPageState createState() => _AdminPageState();
+}
+
+class _AdminPageState extends State<AdminPage> {
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _startLoading();
+  }
+
+  _startLoading() async {
+    await Future.delayed(Duration(seconds: 3));
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
+  void _navigateToEstadisticaPage() {
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: (context) => EstadisticaPage()),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _isLoading
+        ? LoadingScreen()
+        : Scaffold(
+            body: Column(
+              children: [
+                SafeArea(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: Icon(
+                            Icons.arrow_back_ios,
+                            color: Colors.black,
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: _navigateToEstadisticaPage,
+                          child: Icon(
+                            Icons.show_chart,
+                            color: Colors.blue,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20), // Espacio para separar el título de los reportes
+                Padding(
+                  padding: const EdgeInsets.only(left: 20, right: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        'REPORTES TECNOVIGILANCIA',
+                        style: TextStyle(
+                          fontSize: 22, // Texto más pequeño
+                          fontWeight: FontWeight.bold,
+                          color: Colors.red, // Texto en rojo negritas
+                        ),
+                      ),
+                      SizedBox(width: 10), // Espacio entre el texto y el icono
+                      Icon(Icons.report, color: Colors.red), // Icono de reporte en rojo
+                    ],
+                  ),
+                ),
+                SizedBox(height: 20), // Espacio para separar el título de los reportes
+                Expanded(
+                  child: StreamBuilder(
+                    stream: FirebaseFirestore.instance.collection('Formulario').snapshots(),
+                    builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                      if (snapshot.hasError) {
+                        return ErrorScreen();
+                      }
+
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return LoadingScreen();
+                      }
+
+                      if (snapshot.data!.docs.isEmpty) {
+                        return Center(
+                          child: Text(
+                            'No hay reportes registrados',
+                            style: TextStyle(
+                              color: Colors.blue, // Texto en azul
+                            ),
+                          ),
+                        );
+                      }
+
+                      return ListView.builder(
+                        padding: EdgeInsets.all(20),
+                        itemCount: snapshot.data!.docs.length,
+                        itemBuilder: (context, index) {
+                          QueryDocumentSnapshot document = snapshot.data!.docs[index];
+                          Map<String, dynamic> data = document.data() as Map<String, dynamic>;
+                          int numeroReporte = index + 1; // Obtener el número de reporte automáticamente
+                          String reporteTitle = 'Reporte $numeroReporte';
+                          String fechaNotificacion = data['fecha_notificacion'] ?? 'Fecha no disponible'; // Obtener la fecha de notificación del documento
+
+                          return Card(
+                            elevation: 3,
+                            margin: EdgeInsets.symmetric(vertical: 10),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                gradient: LinearGradient(
+                                  colors: [Colors.blue.shade900, Colors.blue.shade300],
+                                  begin: Alignment.topLeft,
+                                  end: Alignment.bottomRight,
+                                ),
+                                borderRadius: BorderRadius.circular(4.0),
+                              ),
+                              child: ListTile(
+                                title: Text(
+                                  reporteTitle,
+                                  style: TextStyle(
+                                    color: Colors.white, // Texto blanco
+                                    fontWeight: FontWeight.bold, // Texto en negritas
+                                  ),
+                                ),
+                                subtitle: Text(
+                                  fechaNotificacion,
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                onTap: () {
+                                  showDialog(
+                                    context: context,
+                                    builder: (BuildContext context) {
+                                      return AlertDialog(
+                                        backgroundColor: Colors.blue.shade50,
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.circular(20.0),
+                                        ),
+                                        title: Text(
+                                          'Detalles del Reporte',
+                                          style: TextStyle(
+                                            color: Colors.blue.shade900,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                        content: SingleChildScrollView(
+                                          child: Column(
+                                            crossAxisAlignment: CrossAxisAlignment.start,
+                                            children: data.entries.map((e) {
+                                              return Padding(
+                                                padding: EdgeInsets.symmetric(vertical: 8.0),
+                                                child: Text(
+                                                  '${e.key}: ${e.value}',
+                                                  style: TextStyle(
+                                                    color: Colors.black, // Texto en negro para facilitar la lectura
+                                                  ),
+                                                ),
+                                              );
+                                            }).toList(),
+                                          ),
+                                        ),
+                                        actions: <Widget>[
+                                          TextButton(
+                                            child: Text(
+                                              'Generar PDF',
+                                              style: TextStyle(color: Colors.blue.shade900),
+                                            ),
+                                            onPressed: () async {
+                                              final pdf = pw.Document();
+                                              pdf.addPage(
+                                                pw.Page(
+                                                  pageFormat: PdfPageFormat.a4,
+                                                  build: (pw.Context context) {
+                                                    return pw.Center(
+                                                      child: pw.Column(
+                                                        crossAxisAlignment: pw.CrossAxisAlignment.start,
+                                                        children: data.entries.map((e) {
+                                                          return pw.Text('${e.key}: ${e.value}');
+                                                        }).toList(),
+                                                      ),
+                                                    );
+                                                  },
+                                                ),
+                                              );
+                                              await Printing.layoutPdf(
+                                                onLayout: (PdfPageFormat format) async => pdf.save(),
+                                              );
+                                            },
+                                          ),
+                                          TextButton(
+                                            child: Text(
+                                              'Cerrar',
+                                              style: TextStyle(color: Colors.blue.shade900),
+                                            ),
+                                            onPressed: () {
+                                              Navigator.of(context).pop();
+                                            },
+                                          ),
+                                        ],
+                                      );
+                                    },
+                                  );
+                                },
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          );
+  }
+}
+
+// Pantalla de carga personalizada
+class LoadingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Admin Page'),
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Container(
+          width: 200,
+          height: 10,
+          child: LinearProgressIndicator(
+            valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
+            backgroundColor: Colors.blue.withOpacity(0.3),
+          ),
+        ),
       ),
+    );
+  }
+}
+
+// Pantalla de error personalizada
+class ErrorScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
       body: Center(
         child: Text(
-          'Bienvenidos, Administrador',
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          'Error al inicializar Firebase',
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+            color: Colors.red,
+          ),
         ),
       ),
     );
---
+++ lib/estadistica.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EstadisticaPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Estadísticas'),
+      ),
+      body: Center(
+        child: Text(''),
+      ),
+    );
+  }
+}
